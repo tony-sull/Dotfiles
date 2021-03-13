f04b15b86f3c108bc643ee050096efb22c3f5e63
Tony Sullivan <tony@navillus.nl>
2021-03-13 15:20:42 +0100

    adding the rest of my WIP config files

---
+++ .local/bin/get-nordvpn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Get the current status of nordvpn
+status=$(nordvpn status | grep -E 'Status: ' | awk -F': ' '{print $2}')
+
+if [ $status == 'Connected' ]; then
+    echo  $(nordvpn status | grep -E 'Country: ' | awk -F': ' '{print $2}')
+else
+    echo  Disconnected
+fi
+
+exit 0
